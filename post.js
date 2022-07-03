d7e38a98fc97146bbd70dd9e2d5a6ae844707752
Kfir Atia <kfir43908@gmail.com>
2022-07-03 22:33:45 +0300

    add all file

--- post.js
+++ post.js
@@ -3,31 +3,57 @@ const express = require("express")
 const cors = require('cors');
 const app = express()
 app.use(cors());
-var x = ""
+const fs = require("fs");
+const { parse } = require("csv-parse");
+app.use(cors());
 
-const client = new Client({
-    host: "47.254.229.61",
-    user: "postgres",
-    port: 5432,
-    password: "DoronKreiz123",
-    database: "saintgraves"
-})
+graves = []
 
-client.connect();
+fs.createReadStream("./rabbis_dataframe_with_coor.csv")
+  .pipe(parse({ delimiter: ",", from_line: 2 }))
+  .on("data", function (row) {
+    graves.push({
+        "name": row[1],
+        "description": row[2],
+        "district": row[3],
+        "city": row[4],
+        "direction":row[5],
+        "latitude" : row[6],
+        "longitude" : row[7]
+    })
+  })
 
-client.query('SELECT * FROM table_name',(err, res)=>{
-    if(!err){
-        x = res.rows
-    }else{
-        console.log(err.message);
-    }
-    client.end
-})
+
+// var data = require("fs").readFileSync("rabbis_dataframe_coor.CSV", "utf8")
+
+// console.log(data["name"]);
+
+// app.get('/api', (req, res) => {
+//     res.send(graves)
+//   });
+// const client = new Client({
+//     host: "47.254.229.61",
+//     user: "postgres",
+//     port: 5432,
+//     password: "DoronKreiz123",
+//     database: "saintgraves"
+// })
+
+// client.connect();
+
+// client.query('SELECT * FROM table_name',(err, res)=>{
+//     if(!err){
+//         x = res.rows
+//     }else{
+//         console.log(err.message);
+//     }
+//     client.end
+// })
 
 app.use(express.static(__dirname));
 
 app.get('/api', (req, res) => {
-    res.send(x)
+    res.send(graves)
   });
 
   app.get('/', (req, res) => {
